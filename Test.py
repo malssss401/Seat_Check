af631998018aac6da16707400518f9a46ca9c5ec
malssss401 <amalaveka@gmail.com>
2026-03-13 00:07:21 +0530

    Test.py

--- Test.py
+++ Test.py
@@ -1,10 +1,19 @@
 import requests
+import os
+from datetime import datetime
+
+PUSHOVER_USER = os.environ["PUSHOVER_USER"]
+PUSHOVER_TOKEN = os.environ["PUSHOVER_TOKEN"]
+
+message = f"✅ GitHub seat monitor test ran successfully at {datetime.utcnow()}"
 
 requests.post(
     "https://api.pushover.net/1/messages.json",
     data={
         "token": PUSHOVER_TOKEN,
         "user": PUSHOVER_USER,
-        "message": "Test notification from ICAI monitor"
+        "message": message
     }
 )
+
+print("Notification sent.")
